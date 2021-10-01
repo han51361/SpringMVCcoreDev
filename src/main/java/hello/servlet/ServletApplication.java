86fc7e5fdaf1eb4e629bddc2f8eeec98662b61a5
xonmin <han51361@naver.com>
2021-10-01 18:03:17 +0900

    MVC 프레임워크 패턴 - 스프링 프레임워크 구조 (핸들러 / 핸들러 어댑터 / 뷰리졸버)

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @ServletComponentScan //서블릿을 직접(자동) 등록해서 사용할 수 있도록
@@ -11,5 +13,7 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+
 
 }
